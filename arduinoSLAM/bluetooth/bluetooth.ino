5e4ce3f8e32dc39011af6e4e83ed10f2e913d84a
Simon Fong <simonfong6@gmail.com>
2017-09-04 12:09:15 -0700

    Added read capability

--- arduinoSLAM/bluetooth/bluetooth.ino
+++ arduinoSLAM/bluetooth/bluetooth.ino
@@ -1,11 +1,17 @@
 
 void setup() {
+  Serial.begin(9600);
   setupBluetooth();
 }
 
 void loop() {
-  
+  //Write to Matlab
   Serial1.println("I love big fat nuts");
+
+  //Read from Matlab
+  if(Serial1.available() > 0){
+    Serial.print(Serial1.readString());
+  }
 }
 
 void setupBluetooth(){
